6094b6b7e7a670d334c3452810e48d3748b9c139
TukTuk-OneLung <102635785+TukTuk-OneLung@users.noreply.github.com>
2025-01-03 20:14:44 -0700

    Update pandas_array.py

--- pandas_array.py
+++ pandas_array.py
@@ -7,24 +7,46 @@ Created on Tue Dec 31 15:13:59 2024
 
 import pandas as pd
 
-puzzle = pd.read_csv('C:/Users/ageffre/Documents/Python/Mayan Puzzle/Mayan Puzzle - Data with no placeholders.csv', names=['A','B','C','D','E','F','G','H','I','J','K','L'])
+puzzle = pd.read_csv('C:/Users/alexg/Documents/Python Scripts/Mayan_Puzzle/Mayan_Puzzle/Calendar_Puzzle/Mayan Puzzle - Data with no placeholders.csv', names=['A','B','C','D','E','F','G','H','I','J','K','L'])
+# puzzle = pd.read_csv('C:/Users/alexg/Documents/Python Scripts/Mayan_Puzzle/Mayan_Puzzle/Calendar_Puzzle/Mayan Puzzle - Data with no placeholders.csv', names=['A','B','C','D','E','F','G','H','I','J','K','L'])
 
 print(puzzle)
+# print(puzzle)
 
 layer5 = puzzle.iloc[16:20]
 layer4 = puzzle.iloc[12:16]
 layer3 = puzzle.iloc[8:12]
 layer2 = puzzle.iloc[4:8]
 layer1 = puzzle.iloc[0:4]
+# layer5 = puzzle.iloc[16:20]
+# layer4 = puzzle.iloc[12:16]
+# layer3 = puzzle.iloc[8:12]
+# layer2 = puzzle.iloc[4:8]
+# layer1 = puzzle.iloc[0:4]
+layer5rows = [16,17,18,19,20]
+layer4rows = [12,13,14,15]
+layer3rows = [8,9,10,11]
+layer2rows = [4,5,6,7]
+layer1rows = [0,1,2,3]
 
+layer5 = pd.read_csv('C:/Users/alexg/Documents/Python Scripts/Mayan_Puzzle/Mayan_Puzzle/Calendar_Puzzle/Mayan Puzzle - Data with no placeholders.csv', names=['A','B','C','D','E','F','G','H','I','J','K','L'], skiprows = lambda x: x not in layer5rows)
+layer4 = pd.read_csv('C:/Users/alexg/Documents/Python Scripts/Mayan_Puzzle/Mayan_Puzzle/Calendar_Puzzle/Mayan Puzzle - Data with no placeholders.csv', names=['A','B','C','D','E','F','G','H','I','J','K','L'], skiprows = lambda x: x not in layer4rows)
+layer3 = pd.read_csv('C:/Users/alexg/Documents/Python Scripts/Mayan_Puzzle/Mayan_Puzzle/Calendar_Puzzle/Mayan Puzzle - Data with no placeholders.csv', names=['A','B','C','D','E','F','G','H','I','J','K','L'], skiprows = lambda x: x not in layer3rows)
+layer2 = pd.read_csv('C:/Users/alexg/Documents/Python Scripts/Mayan_Puzzle/Mayan_Puzzle/Calendar_Puzzle/Mayan Puzzle - Data with no placeholders.csv', names=['A','B','C','D','E','F','G','H','I','J','K','L'], skiprows = lambda x: x not in layer2rows)
+layer1 = pd.read_csv('C:/Users/alexg/Documents/Python Scripts/Mayan_Puzzle/Mayan_Puzzle/Calendar_Puzzle/Mayan Puzzle - Data with no placeholders.csv', names=['A','B','C','D','E','F','G','H','I','J','K','L'], skiprows = lambda x: x not in layer1rows)
 
+# layer2 = 
+# layer1 = 
+
+print('')
+print('')
 print(layer5)
 print('')
 print(layer4)
 print('')
 print(layer3)
-print('')
 print(layer2)
 print('')
 print(layer1)
 print('')
+
