36e951d3026a76b29c860aa8b4ebe9b256b061f4
TukTuk-OneLung <102635785+TukTuk-OneLung@users.noreply.github.com>
2025-01-04 18:57:37 -0700

    Almost there

--- pandas_array.py
+++ pandas_array.py
@@ -13,6 +13,11 @@ column2 = 0
 column3 = 0
 column4 = 0
 column5 = 0
+activecolumn1 = 0
+activecolumn2 = 0
+activecolumn3 = 0
+activecolumn4 = 0
+activecolumn5 = 0
 cell1 = 0
 cell2 = 0
 cell3 = 0
@@ -74,133 +79,202 @@ print(layer1)
 print("")
 
 
-
+################################################################################################
+# This section takes the active column in each row and stacks the values on top of each other. #
+# The highest numbered layer with a value is passed to that cell.                              #
+################################################################################################
 
 def stack():
     
-    global column5
-    global column4
-    global column3
-    global column2
-    global column1
+    global activecolumn5
+    global activecolumn4
+    global activecolumn3
+    global activecolumn2
+    global activecolumn1
     
-    # Layer row 3 of the defined column and set cell1 to the value that would show in that orientation
-    global cell1
-    if (layer5.iat[3,column5] > 0):
-        cell1 = layer5.iat[3,column5]
-    elif (layer4.iat[3,column4] > 0):
-        cell1 = layer4.iat[3,column4]
-    elif (layer3.iat[3,column3] > 0):
-        cell1 = layer3.iat[3,column3]
-    elif (layer2.iat[3,column2] > 0):
-        cell1 = layer2.iat[3,column2]
+    global cell1 # This is the innermost ring of the puzzle. Values from all layers can exist here.
+    if (layer5.iat[3,activecolumn5] > 0):
+        cell1 = layer5.iat[3,activecolumn5]
+    elif (layer4.iat[3,activecolumn4] > 0):
+        cell1 = layer4.iat[3,activecolumn4]
+    elif (layer3.iat[3,activecolumn3] > 0):
+        cell1 = layer3.iat[3,activecolumn3]
+    elif (layer2.iat[3,activecolumn2] > 0):
+        cell1 = layer2.iat[3,activecolumn2]
     else: 
-        cell1 = layer1.iat[3,column1]
+        cell1 = layer1.iat[3,activecolumn1]
 
-    global cell2        
-    if (layer5.iat[2,column5] > 0):
-        cell2 = layer5.iat[2,column5]
-    elif (layer4.iat[2,column4] > 0):
-        cell2 = layer4.iat[2,column4]
-    elif (layer3.iat[2,column3] > 0):
-        cell2 = layer3.iat[2,column3]
-    elif (layer2.iat[2,column2] > 0):
-        cell2 = layer2.iat[2,column2]
+    global cell2 # This is second ring of the puzzle from the center. Values from all layers except 5 can exist here. 
+    if (layer5.iat[2,activecolumn5] > 0):
+        cell2 = layer5.iat[2,activecolumn5]
+    elif (layer4.iat[2,activecolumn4] > 0):
+        cell2 = layer4.iat[2,activecolumn4]
+    elif (layer3.iat[2,activecolumn3] > 0):
+        cell2 = layer3.iat[2,activecolumn3]
+    elif (layer2.iat[2,activecolumn2] > 0):
+        cell2 = layer2.iat[2,activecolumn2]
     else: 
-        cell2 = layer1.iat[2,column1]
+        cell2 = layer1.iat[2,activecolumn1]
 
-    global cell3
-    if (layer5.iat[1,column5] > 0):
-        cell3 = layer5.iat[1,column5]
-    elif (layer4.iat[1,column4] > 0):
-        cell3 = layer4.iat[1,column4]
-    elif (layer3.iat[1,column3] > 0):
-        cell3 = layer3.iat[1,column3]
-    elif (layer2.iat[1,column2] > 0):
-        cell3 = layer2.iat[1,column2]
+    global cell3 # This is the 3rd ring of the puzzle from the center. Values from Layers 1-3 can exist here.
+    if (layer5.iat[1,activecolumn5] > 0):
+        cell3 = layer5.iat[1,activecolumn5]
+    elif (layer4.iat[1,activecolumn4] > 0):
+        cell3 = layer4.iat[1,activecolumn4]
+    elif (layer3.iat[1,activecolumn3] > 0):
+        cell3 = layer3.iat[1,activecolumn3]
+    elif (layer2.iat[1,activecolumn2] > 0):
+        cell3 = layer2.iat[1,activecolumn2]
     else: 
-        cell3 = layer1.iat[1,column1]
+        cell3 = layer1.iat[1,activecolumn1]
  
     
-    global cell4    
-    if (layer5.iat[0,column5] > 0):
-        cell4 = layer5.iat[0,column5]
-    elif (layer4.iat[0,column4] > 0):
-        cell4 = layer4.iat[0,column4]
-    elif (layer3.iat[0,column3] > 0):
-        cell4 = layer3.iat[0,column3]
-    elif (layer2.iat[0,column2] > 0):
-        cell4 = layer2.iat[0,column2]
+    global cell4 # This is the outermost ring of the puzzle. Only values from layers 1 and 2 can exist here.  
+    if (layer5.iat[0,activecolumn5] > 0):
+        cell4 = layer5.iat[0,activecolumn5]
+    elif (layer4.iat[0,activecolumn4] > 0):
+        cell4 = layer4.iat[0,activecolumn4]
+    elif (layer3.iat[0,activecolumn3] > 0):
+        cell4 = layer3.iat[0,activecolumn3]
+    elif (layer2.iat[0,activecolumn2] > 0):
+        cell4 = layer2.iat[0,activecolumn2]
     else: 
-        cell4 = layer1.iat[0,column1]
+        cell4 = layer1.iat[0,activecolumn1]
         
-        
-def index5():
+########################################################################################
+# This section is a rolling index that works similar to an analog odometer.            #
+# When layer 5 index rolls over to 0, layer 4 is indexed by one.                       # 
+# The process repeats with layers 3, 2, and 1 as the layer above is indexed back to 0. #
+########################################################################################
+
+def index5(): # Start by rotating the topmost layer of the puzzle one at a time.
     
     global column5
     
-    if (column5 <=11):
+    if (column5 < 11):
         column5 = (column5 + 1)
     else:
         column5 = 0
         index4()
     
-def index4():
+def index4(): # Rotate the second layer from the top one position as layer 5 indexes back to 0.
     
     global column4
     
-    if (column4 <=11):
-        column4 = (column5 + 1)
+    if (column4 < 11):
+        column4 = (column4 + 1)
     else:
         column4 = 0
         index3()
     
-def index3():
+def index3(): # Rotate the third layer from the top one position as layer 4 indexes back to 0.
     
     global column3
     
-    if (column3 <=11):
-        column3 = (column5 + 1)
+    if (column3 < 11):
+        column3 = (column3 + 1)
     else:
         column3 = 0
         index2()
     
-def index2():
+def index2(): # Rotate the fourth layer from the top one position as layer 3 indexes back to 0.
     
     global column2
     
-    if (column2 <=11):
-        column2 = (column5 + 1)
+    if (column2 < 11):
+        column2 = (column2 + 1)
     else:
         column2 = 0
         index1()
 
-def index1():
+def index1(): # Rotate the bottom layer one position as layer 2 indexes back to 0.
 
     global column1    
     
-    if (column1 <=11):
-        column1 = (column5 + 1)
+    if (column1 < 11):
+        column1 = (column1 + 1)
     else:
         column1 = 0
         print("Analysis failed")
         
-
+def nextcolumn():
+    
+    global activecolumn1
+    global activecolumn2
+    global activecolumn3
+    global activecolumn4
+    global activecolumn5
+    
+    if (activecolumn1 < 11):
+        activecolumn1 = (activecolumn1 + 1)
+    else:
+        activecolumn1 = 0
         
-    
-stack()
-print(cell1)
-print(cell2)
-print(cell3)
-print(cell4)
+    if (activecolumn2 < 11):
+        activecolumn2 = (activecolumn2 + 1)
+    else:
+        activecolumn2 = 0
 
+    if (activecolumn3 < 11):
+        activecolumn3 = (activecolumn3 + 1)
+    else:
+        activecolumn3 = 0
 
-while solved != False:
-    stack()
+    if (activecolumn4 < 11):
+        activecolumn4 = (activecolumn4 + 1)
+    else:
+        activecolumn4 = 0
+
+    if (activecolumn5 < 11):
+        activecolumn5 = (activecolumn5 + 1)
+    else:
+        activecolumn5 = 0
+
     
-    if (sum(cell1,cell2,cell3,cell4) == 42):
+# stack()
+# print(cell1)
+# print(cell2)
+# print(cell3)
+# print(cell4)
+
+    
+    
+def checkresult():
+    
+    global passcount
+    global cell1
+    global cell2
+    global cell3
+    global cell4
+    global solved
+    global activecolumn1
+    global activecolumn2
+    global activecolumn3
+    global activecolumn4
+    global activecolumn5
+    global column1
+    global column2
+    global column3
+    global column4
+    global column5
+    global pass1
+    global pass2
+    global pass3
+    global pass4
+    global pass5
+    global pass6
+    global pass7
+    global pass8
+    global pass9
+    global pass10
+    global pass11
+    global pass12
+    
+    
+    if (cell1 + cell2 + cell3 + cell4) == 42:
         passcount = (passcount + 1)
         print("Passed ", passcount, " out of 12.")
+        nextcolumn()
         if (passcount == 1):
             pass1 = [cell1,cell2,cell3,cell4]
         elif (passcount == 2):
@@ -251,11 +325,32 @@ while solved != False:
             print(pass11)
             print("Column L")
             print(pass12)
+            print("Bottom Layer")
+            print(column1)
+            print("Second Layer")
+            print(column2)
+            print("Third Layer")
+            print(column3)
+            print("Fourth Layer")
+            print(column4)
+            print("Fifth Layer")
+            print(column5)
             
             
     else:
+        print("No match. Indexing...")
         index5()
+        activecolumn5 = column5
+        activecolumn4 = column4
+        activecolumn3 = column3
+        activecolumn2 = column2
+        activecolumn1 = column1
         
         
-        #function that shifts layer 5 to the next column
+while solved == False:
+
+    stack()
+    print("Stacking")
+    checkresult()
+    print("Checking")
     
